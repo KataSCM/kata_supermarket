197697f589398d3f55d22397e9ab888184200dea
Miguel Olmos <mgl.olmos@gmail.com>
2015-03-25 13:50:47 +0100

    Fix format SupplementMarketInterface in PHP

--- php/SupplementMarketInterface.php
+++ php/SupplementMarketInterface.php
@@ -5,20 +5,20 @@
  */
 interface SupplementMarketInterface
 {
-	/**
-	 * Get tax of product in percentage.
-	 *
-	 * @return integer
-	 */
-	public function getTax($product);
+    /**
+     * Get tax of product in percentage.
+     *
+     * @return integer
+     */
+    public function getTax($product);
 
-	/**
-	 * Get offer of product if exists.
-	 *
-	 * If the product has offer, it will return a array where will indicate what offer apply. Ej. 2x1 -> array(2, 1), 3x2 -> array(3,2).
-	 * If the product does not have offer, it will return null.
-	 *
-	 * @return array|null
-	 */
-	public function getOffer($product);
+    /**
+     * Get offer of product if exists.
+     *
+     * If the product has offer, it will return a array where will indicate what offer apply. Ej. 2x1 -> array(2, 1), 3x2 -> array(3,2).
+     * If the product does not have offer, it will return null.
+     *
+     * @return array|null
+     */
+    public function getOffer($product);
 }
